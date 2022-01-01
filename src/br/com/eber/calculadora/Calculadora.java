fc199465e0316f1c982df5b6ab46323a46dda387
Eber Oliveira <eber.oliv.costa@gmail.com>
2022-01-01 11:19:51 -0300

    Criando Classe Calculadora

---
+++ src/br/com/eber/calculadora/Calculadora.java
@@ -0,0 +1,42 @@
+package br.com.eber.calculadora;
+
+import java.util.Scanner;
+
+public class Calculadora {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+         int a, b;
+
+        System.out.println("Digite o primeiro numero: ");
+        a = scan.nextInt();
+        System.out.println("Digite o segundo numero: ");
+        b = scan.nextInt();
+
+        int somar = somar(a, b);
+        int subtrair = subtrair(a, b);
+        int multiplicar = multiplicar(a, b);
+        int dividir = dividir(a, b);
+
+        System.out.println("Somar: " + somar);
+        System.out.println("Sub: " + subtrair);
+        System.out.println("Multi: " + multiplicar);
+        System.out.println("Div: " + dividir);
+
+
+    }
+
+    public static int somar (int a, int b){
+        return a + b;
+    }
+    public static int subtrair (int a, int b){
+        return a - b;
+    }
+    public static int multiplicar (int a, int b){
+        return a * b;
+    }
+
+    public static int dividir (int a, int b){
+        return a / b;
+    }
+}
